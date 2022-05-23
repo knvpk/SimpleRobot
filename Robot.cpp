c841377bb16f33378ace9aafaf01a3f9980e9047
Pavan Katakam <pkatakam@greenstreet.com>
2022-05-24 00:01:09 +0530

    connection to base from robot is missing

--- Robot.cpp
+++ Robot.cpp
@@ -6,10 +6,13 @@
 
 using namespace std;
 
-// Robot ::Robot(string serial, int numWheels, string LiDARType, bool withCam, int inital_X, int inital_Y)
-// {
-//     serialNumber = serial;
-// }
+
+Robot ::Robot(string serial, int numWheels, string LiDARType, bool withCam, int inital_X, int inital_Y)
+{
+    MobileBase *base = new MobileBase(numWheels, LiDARType, withCam, inital_X, inital_Y);
+    serialNumber = serial;
+    numberofRobotsExisiting = numWheels;
+}
 
 void Robot::ShowSerialNo()
 {
